fc932ae8d1a495d73c38017f856776cba7918c58
Naman Parashar <nparashar150@gmail.com>
2024-03-20 09:27:04 +0530

    move util fn to main

--- dockerfile
+++ dockerfile
@@ -4,5 +4,3 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install fastapi openai slowapi python-dotenv requests uvicorn python-multipart
 COPY ./ /code/app
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
-
-  
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from dotenv import load_dotenv
 import os
-from util import segments_to_srt
+from datetime import timedelta
 
 load_dotenv()
 
@@ -28,6 +28,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def segments_to_srt(segments):
+    srt_content = ""  # Initialize an empty string to store the SRT content
+
+    for segment in segments:
+        startTime = str(0) + str(timedelta(seconds=int(segment['start']))) + ',000'
+        endTime = str(0) + str(timedelta(seconds=int(segment['end']))) + ',000'
+        text = segment['text']
+        segmentId = segment['id'] + 1
+        segment_srt = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
+        srt_content += segment_srt  # Append each segment's SRT content to the main string
+
+    print("SRT content generated successfully")
+    return srt_content  # Return the accumulated SRT content directly
+
 
 @app.get("/")
 def read_root():
